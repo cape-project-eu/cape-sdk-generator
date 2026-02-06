596248d14f972fed6f32a666ca4435ad58b4e0ff
Christoph Bühler <christoph.buehler@unisg.ch>
2026-02-06 11:46:25 +0100

    feat(examples): add nodejs example

---
+++ examples/pulumi_nodejs/index.ts
@@ -0,0 +1,12 @@
+import * as cape from '@pulumi/cape';
+
+const ws = new cape.workspace.Workspace('myWorkspace', { spec: {} }, {});
+const bs = new cape.storage.BlockStorage('myStorage', {
+  workspace: ws.metadata.apply((m) => m.name),
+  spec: {
+    sizeGB: 32,
+    skuRef: {
+      referenceURN: 'my-sku-ref',
+    },
+  },
+});
